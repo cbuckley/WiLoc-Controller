8f48f8967a104ceeb82eca486a3f12acda4ebf58
root <root@BLUEPI.wiloc.magma.local>
2014-04-12 14:11:59 +0000

    Added multi-controller identification through controller.cfg

--- heartbeat.pl
+++ heartbeat.pl
@@ -4,7 +4,15 @@ use Net::Stomp;
 use JSON;
 my $stomp = Net::Stomp->new( { hostname => '192.168.10.99', port => '61613' } );
 my $connection = $stomp->connect( { login => 'admin', passcode => 'password' } );
-my $controllerid = "lrhcngawjz";
+open DATA, "controller.cfg" or die "Can't open file: $!";
+my $controllerid;
+while (<DATA>) {
+	chomp;
+	($controllerid) = split /\|/;
+}
+close DATA;
+
+die "OOPS" if !$controllerid;
 
 while($connection)	{
 	my $heartbeat = {controller=>$controllerid, ip=>"192.168.22.2", time=>time()};
